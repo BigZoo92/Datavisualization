9fc2b65277b3c71fccdfa42e08a3267f9cfbd048
BigZoo92 <givernaudenzo@gmail.com>
2023-01-10 04:03:12 +0100

    3d map + points

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,49 +1,112 @@
-let tKeys = [];
-let tValues = [];
-let index = -1;
-let geolocation;
-let dataset = [];
-d3.json("https://data.nasa.gov/resource/gh4g-9sfh.json")
-      .then(function(data) {
-        for (let i = 0; i < 10; i++) {
-          geolocation = JSON.stringify(data[i]["geolocation"]);
-          geolocation = geolocation.replace("{", "")
-          geolocation = geolocation.replace("}", "")
-          console.log(geolocation);
-          data[i]["geolocation"] = geolocation
-          Object.entries(data[i]).forEach(e => {
-            dataset.push(e);
-          });        
-        }
-          d3.select('.container')
-            .selectAll('p')
-            .data(dataset)
-            .enter()
-            .append('p')
-            .text( d => {
-              return d
-            })
-            document.querySelectorAll('p').forEach((e) => {
-              e.innerHTML = e.innerHTML.replace(",", " : ")
-            })
-       });
-const width = 900;
-const height = 600;
+const width = 600;
+const height = width;
 const inset = 20;
-const outline = ({type: "Sphere"})
+const points = [];
+const vx = 0.001, vy = -0.001;
+// versor = require("versor@0.0.3");
+const sphere = ({type: "Sphere"})
 const graticule = d3.geoGraticule10()
-const svg = d3.select('body').append('svg').attr('width', width).attr('height', height);
 
-const projection = d3.geoOrthographic().fitExtent([[inset, inset], [width - inset, height - inset]], outline)
-const path = d3.geoPath(projection);
+const canvas = d3.select('body').append('canvas').attr('width', width).attr('height', height);
 
-const g = svg.append('g')
+const projection = d3.geoOrthographic()
+.fitExtent([[-1, -1], [width + 1, height + 1]], sphere)
+.clipExtent([[-1, -1], [width + 1, height + 1]])
+.rotate([0, -30]);
+const context = canvas.node().getContext('2d')
+const path = d3.geoPath(projection, context)
+      .pointRadius(2.5);
+function tester(projection) {
+  let visible;
+  const stream = projection.stream({point() { visible = true; }});
+  return ([x, y]) => (visible = false, stream.point(x, y), visible);
+}
 
-d3.json('https://s3-us-west-2.amazonaws.com/s.cdpn.io/95802/world-110m.json')
-  .then(data =>{
-    console.log(data, data.objects.countries);
-    const countries = topojson.feature(data, data.objects.countries)
-    console.log(countries)
-    g.selectAll('path').data(countries.features).enter().append('path').attr('class', 'country').attr('d', path)
+function drag(projection) {
+  let v0, q0, r0, a0, l;
+
+  function pointer(event, that) {
+    const t = d3.pointers(event, that);
+
+    if (t.length !== l) {
+      l = t.length;
+      if (l > 1) a0 = Math.atan2(t[1][1] - t[0][1], t[1][0] - t[0][0]);
+      dragstarted.apply(that, [event, that]);
+    }
+
+    // For multitouch, average positions and compute rotation.
+    if (l > 1) {
+      const x = d3.mean(t, p => p[0]);
+      const y = d3.mean(t, p => p[1]);
+      const a = Math.atan2(t[1][1] - t[0][1], t[1][0] - t[0][0]);
+      return [x, y, a];
+    }
+
+    return t[0];
+  }
+
+  function dragstarted(event) {
+    v0 = versor.cartesian(projection.invert(pointer(event, this)));
+    q0 = versor(r0 = projection.rotate());
+  }
+
+  function dragged(event) {
+    const p = pointer(event, this);
+    const v1 = versor.cartesian(projection.rotate(r0).invert(p));
+    const delta = versor.delta(v0, v1);
+    let q1 = versor.multiply(q0, delta);
+
+    // For multitouch, compose with a rotation around the axis.
+    if (p[2]) {
+      const d = (p[2] - a0) / 2;
+      const s = -Math.sin(d);
+      const c = Math.sign(Math.cos(d));
+      q1 = versor.multiply([Math.sqrt(1 - s * s), 0, 0, c * s], q1);
+    }
+
+    projection.rotate(versor.rotation(q1));
+
+    // In vicinity of the antipode (unstable) of q0, restart.
+    if (delta[0] < 0.7) dragstarted.apply(this, [event, this]);
+  }
+
+  return d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged);
+}
+
+d3.json('land-50m.json')
+  .then(data1 =>{
+    const land50 = topojson.feature(data1, data1.objects.land)
+    d3.json('land-110m.json')
+      .then(data2 =>{
+        const land110 = topojson.feature(data2, data2.objects.land)
+        d3.json('https://data.nasa.gov/resource/gh4g-9sfh.json')
+          .then(data3 =>{
+            for (let i = 0; i < 1000; i++) {
+              if (data3[i]["geolocation"] !== undefined) {
+                points.push(Object.values(data3[i]["geolocation"]));
+              } 
+              
+            }
+            console.log(data3[147]["geolocation"]);
+            function render(land) {
+              context.clearRect(0, 0, width, height);
+              context.beginPath(), path(sphere), context.fillStyle = "#fff", context.fill();
+              context.beginPath(), path(land), context.fillStyle = "#000", context.fill();
+              context.beginPath(), path(sphere), context.stroke();
+              context.beginPath(), path({type: "MultiPoint", coordinates: points}), context.fillStyle = "#04E762", context.fill();
+            }
+            
+            return d3.select(context.canvas)
+              .call(drag(projection)
+                  .on("drag.render", () => render(land110))
+                  .on("end.render", () => render(land50)))
+              .call(() => render(land50))
+              .node();
+        })
+    })
 })
+
+// tar -xvzf C:\Users\giver\Downloads\testing-projection-visibility.tgz -C C:\Users\giver\Desktop\testd3
 
